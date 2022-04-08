b2e3914c754b032b5f942d86b4aee35ce1e1feaa
Luca Carlig <luca.carlig@stacktical.com>
2022-04-08 14:59:48 +0100

    implement SLO logic functions

--- programs/SLO-registry/src/lib.rs
+++ programs/SLO-registry/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use std::collections::HashMap;
 
-// TODO: deal with modifier
+// FIXME: deal with modifier only SLARegistry can call the functions
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -9,42 +9,83 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod slo_registry {
     use super::*;
 
-    pub fn register_slo(
-        ctx: Context<SLORegistry>,
-        slo_value: u128,
-        slo_type: SLOType,
-        sla_address: Pubkey,
-    ) -> Result<()> {
+    // FIXME: function should be called only once on deployment
+    pub fn set_sla_registry(ctx: Context<Registry>, sla_registry: Pubkey) -> Result<()> {
+        ctx.accounts.slo_registry.sla_registry = sla_registry;
+        ctx.accounts.slo_registry.registered_slo = HashMap::new();
         Ok(())
-    }
-
-    pub fn is_respected(
-        ctx: Context<SLORegistry>,
-        value: u128,
-        sla_address: Pubkey,
-    ) -> Result<bool> {
-        Ok(true)
-    }
-
-    pub fn get_deviation(
-        ctx: Context<SLORegistry>,
-        sli: u128,
-        sla_address: Pubkey,
-        precision: u128,
-    ) -> Result<u128> {
-        Ok(0)
     }
 }
 
 #[derive(Accounts)]
-pub struct SLORegistry<'info> {
+pub struct Registry<'info> {
+    #[account(init, payer = owner, space = 1024)]
+    pub slo_registry: Account<'info, SLORegistry>,
     #[account(mut)]
-    pub slo_registry: AccountInfo<'info>,
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct SLORegistry {
     pub registered_slo: HashMap<Pubkey, SLO>,
     sla_registry: Pubkey,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
+impl SLORegistry {
+    pub fn register_slo(&mut self, sla_address: Pubkey, slo: SLO) -> Result<()> {
+        self.registered_slo.insert(sla_address, slo);
+        Ok(())
+    }
+
+    pub fn is_respected(&self, value: u128, sla_address: Pubkey) -> Result<bool> {
+        let registered_slo = match self.registered_slo.get(&sla_address) {
+            Some(map) => map,
+            None => return err!(ErrorCode::SLONotFound),
+        };
+
+        let slo_type = registered_slo.slo_type;
+        let slo_value = registered_slo.slo_value;
+
+        match slo_type {
+            SLOType::EqualTo => Ok(value == slo_value),
+            SLOType::NotEqualTo => Ok(value != slo_value),
+            SLOType::SmallerThan => Ok(value < slo_value),
+            SLOType::SmallerOrEqualTo => Ok(value <= slo_value),
+            SLOType::GreaterThan => Ok(value > slo_value),
+            SLOType::GreaterOrEqualTo => Ok(value >= slo_value),
+        }
+    }
+
+    pub fn get_deviation(&self, sli: u128, sla_address: Pubkey, precision: u128) -> Result<u128> {
+        let registered_slo = match self.registered_slo.get(&sla_address) {
+            Some(map) => map,
+            None => return err!(ErrorCode::SLONotFound),
+        };
+
+        let slo_type = registered_slo.slo_type;
+        let slo_value = registered_slo.slo_value;
+
+        let mut deviation: u128 = (if sli >= slo_value {
+            sli - slo_value
+        } else {
+            slo_value
+        }) * precision
+            / ((sli + slo_value) / 2);
+
+        if deviation > (precision * 25 / 100) {
+            deviation = precision * 25 / 100;
+        }
+        match slo_type {
+            SLOType::EqualTo => Ok(precision / 100),
+            SLOType::NotEqualTo => Ok(precision / 100),
+            _ => Ok(deviation),
+        }
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum SLOType {
     EqualTo,
     NotEqualTo,
@@ -55,12 +96,25 @@ pub enum SLOType {
 }
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
 pub struct SLO {
-    sloValue: u128,
-    sloType: SLOType,
+    pub slo_value: u128,
+    pub slo_type: SLOType,
+}
+
+impl SLO {
+    pub fn new(slo_value: u128, slo_type: SLOType) -> Self {
+        Self {
+            slo_value,
+            slo_type,
+        }
+    }
 }
 
 #[error_code]
 pub enum ErrorCode {
     #[msg("You are not authorized to perform this action.")]
     Unauthorized,
+    #[msg("the SLA address provided does not have a SLO registered.")]
+    SLONotFound,
+    #[msg("isRespected wasn't executed properly.")]
+    IsRespectedFailed,
 }
