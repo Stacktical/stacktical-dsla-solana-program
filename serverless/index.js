24c799f4370821a212890f679e7de0ef788c8acd
lucarlig <luca.carlig@stacktical.com>
2022-08-19 14:41:46 +0100

    gcloud remove async

--- serverless/index.js
+++ serverless/index.js
@@ -1,36 +1,41 @@
 import got from "got";
 import moment from "moment";
+const functions = require("@google-cloud/functions-framework");
 
 const ENDPOINT = "https://status.solana.com/api/v2/incidents.json";
 
-exports.monthlyUptime = async (req, res) => {
-  const json = await got.get(ENDPOINT).json();
+functions.http("monthlyUptime", (req, res) => {
+  got
+    .get(ENDPOINT)
+    .json()
+    .then((json) => {
+      const incidents = json.incidents;
+      const lastMonth = moment().subtract(1, "month");
 
-  const incidents = json.incidents;
-  const lastMonth = moment().subtract(1, "month");
+      let monthlyUptime =
+        lastMonth.endOf("month").unix() - lastMonth.startOf("month").unix();
+      let monthlyDowntime = 0;
 
-  let monthlyUptime =
-    lastMonth.endOf("month").unix() - lastMonth.startOf("month").unix();
-  let monthlyDowntime = 0;
-
-  for (let incident of incidents) {
-    let startDate = moment(incident.started_at);
-    let endDate = moment(incident.resolved_at);
-    if (
-      incident.impact === "major" &&
-      (startDate.month === lastMonth.month || endDate.month === lastMonth.month)
-    ) {
-      if (startDate.isBefore(lastMonth.startOf("month"))) {
-        startDate = lastMonth.startOf("month");
+      for (let incident of incidents) {
+        let startDate = moment(incident.started_at);
+        let endDate = moment(incident.resolved_at);
+        if (
+          incident.impact === "major" &&
+          (startDate.month === lastMonth.month ||
+            endDate.month === lastMonth.month)
+        ) {
+          if (startDate.isBefore(lastMonth.startOf("month"))) {
+            startDate = lastMonth.startOf("month");
+          }
+          if (endDate.isAfter(lastMonth.endOf("month"))) {
+            endDate = lastMonth.endOf("month");
+          }
+          monthlyDowntime = monthlyUptime + (endDate.unix() - startDate.unix());
+          monthlyUptime = monthlyUptime - monthlyDowntime;
+        }
       }
-      if (endDate.isAfter(lastMonth.endOf("month"))) {
-        endDate = lastMonth.endOf("month");
-      }
-      monthlyDowntime = monthlyUptime + (endDate.unix() - startDate.unix());
-      monthlyUptime = monthlyUptime - monthlyDowntime;
-    }
-  }
 
-  res.status(200).send((monthlyDowntime / monthlyUptime) * 100);
-  res.end();
-};
+      res.status(200).send((monthlyDowntime / monthlyUptime) * 100);
+      res.end();
+    });
+});
